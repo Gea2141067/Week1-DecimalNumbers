7adecb65abb6f6d25b6f819bce4698547a25363c
Gean-Pierre Fernandez Perez <GEA2141067@maricopa.edu>
2020-10-29 20:10:30 -0700

    template in c++ for assigment 1

---
+++ Week1-DecimalNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Week1_DecimalNumbers
+{
+    class Program
+    {
+        static void Main(String[] args_)
+        {
+            var decimalNumber = 239;
+            //Step one you need to get each digit
+            //The first Step is to Convert it to Value to String -> Character
+            //So that you can get to each charecter 2 3 and 9
+
+            var characterArray = decimalNumber.ToString().ToCharArray();
+
+            //Test to see fi this is what you want
+            foreach( var item in characterArray)
+            {
+                //now print each character to console
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
